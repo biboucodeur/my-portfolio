b484b9ac14533c0fbfd646fa7d35a4b02b7e6a05
Abibou wade <abibouwade2298@gmail.com>
2024-10-16 18:41:26 +0000

    footer info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
-          <p>&copy; 2024 Abibou wade. All rights reserved.</p>
+          <p>&copy; 2024 Abibou wade. Tous droits réservés.</p>
           <div className="flex space-x-4">
             <a
               href="https://github.com"
